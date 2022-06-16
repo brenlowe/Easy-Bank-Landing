8803d146ff02ad693242981601a8fabad36aac29
Brennon Lowe <97804096+brenlowe@users.noreply.github.com>
2022-06-16 13:26:33 -0400

    OverlayMenu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,29 @@
-console.log("hello world~!");
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function(){
+
+    if(header.classList.contains('open')){ //close menu
+        body.classList.remove("noscroll");
+        header.classList.remove('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove("fade-in");
+            element.classList.add("fade-out");
+        });
+       
+
+    } else { //open menu
+        body.classList.add("noscroll");
+        header.classList.add('open');
+        fadeElems.forEach(function(element){
+            element.classList.remove("fade-out");
+            element.classList.add("fade-in");
+            element.classList.remove("has-fade");
+        });
+        
+    }
+})
+
